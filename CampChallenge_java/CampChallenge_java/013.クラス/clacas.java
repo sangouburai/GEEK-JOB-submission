b8a4db802925d90948616d0f9b7e7c324a95cb49
sangouburai <36286196+sangouburai@users.noreply.github.com>
2018-03-26 15:52:31 +0900

    Add files via upload

---
+++ CampChallenge_java/CampChallenge_java/013.クラス/clacas.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nfaa;
+
+import java.io.PrintWriter;
+
+/**
+ *
+ * @author guest1Day
+ */
+
+class clacas {
+    
+     String poku = "";
+     int Lr = 0;
+
+   
+     void poktrian(String knbn, int a) {
+     
+        this.poku = knbn;
+        this.Lr = a;}
+        
+     void scre(PrintWriter out){
+        out.print(poku);
+        out.print(Lr);
+        
+    } 
+      }
+     
+class jir{
+
+void gt(String[] hdd) {
+clacas FG = new clacas();
+
+FG.Lr = 61;
+
+
+        }
+}
+
+
+    
+
