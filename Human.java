bedda4485f1c11e0ea5aac3b33af6935c0b1bf7b
sangouburai <36286196+sangouburai@users.noreply.github.com>
2018-03-29 17:27:27 +0900

    Add files via upload

---
+++ Human.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BLCJK;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author guest1Day
+ */
+public class Human {
+    public ArrayList<Integer> mycards = new ArrayList<Integer>();
+    
+    int open(){
+        return 1;
+    }
+    void setCard(){
+        
+    }
+    boolean checkSum(){
+       
+        
+        return true;
+    }
+}
+
+class User extends Human{
+    
+    
+}
